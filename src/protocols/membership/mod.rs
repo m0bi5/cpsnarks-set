c126ac17a1c3fb57ca3ca779871f64f7ba8f9661
Mohit Bhasi <mohitbhasi1998@gmail.com>
2022-01-20 11:07:30 -0500

    Update mod.rs

--- src/protocols/membership/mod.rs
+++ src/protocols/membership/mod.rs
@@ -1,7 +1,6 @@
 //! Implements CPMemRSA and CPMemRSAPrm.
 use crate::{
-    commitments::{integer::IntegerCommitment, pedersen::PedersenCommitment, Commitment},
-    parameters::Parameters,
+    commitments::{integer::IntegerCommitment, pedersen::PedersenCommitment},
     protocols::{
         hash_to_prime::{
             channel::{HashToPrimeProverChannel, HashToPrimeVerifierChannel},
@@ -26,7 +25,24 @@ use crate::{
 use channel::{MembershipProverChannel, MembershipVerifierChannel};
 use rand::{CryptoRng, RngCore};
 use rug::rand::MutRandState;
+use crate::{
+    commitments::Commitment,
+    parameters::Parameters,
+    protocols::hash_to_prime::snark_range::Protocol as HPProtocol,
+    protocols::{
+        hash_to_prime::snark_hash::{HashToPrimeHashParameters, Protocol as HPHashProtocol},
+        membership::transcript::{TranscriptProverChannel, TranscriptVerifierChannel},
+    },
+};
+use accumulator::group::{ClassGroup, Rsa2048};
+use accumulator::{group::Group, AccumulatorWithoutHashToPrime};
+use ark_bls12_381::{Bls12_381, G1Projective};
+use merlin::Transcript;
+use rand::thread_rng;
+use rug::rand::RandState;
 use rug::Integer;
+use std::cell::RefCell;
+
 
 pub mod channel;
 pub mod transcript;
@@ -245,344 +261,80 @@ impl<G: ConvertibleUnknownOrderGroup, P: CurvePointProjective, HP: HashToPrimePr
     }
 }
 
-#[cfg(all(test, feature = "arkworks"))]
-mod test {
-    use super::{Protocol, Statement, Witness};
-    use crate::{
-        commitments::Commitment,
-        parameters::Parameters,
-        protocols::hash_to_prime::snark_range::Protocol as HPProtocol,
-        protocols::{
-            hash_to_prime::snark_hash::{HashToPrimeHashParameters, Protocol as HPHashProtocol},
-            membership::transcript::{TranscriptProverChannel, TranscriptVerifierChannel},
-        },
-    };
-    use accumulator::group::{ClassGroup, Rsa2048};
-    use accumulator::{group::Group, AccumulatorWithoutHashToPrime};
-    use ark_bls12_381::{Bls12_381, G1Projective};
-    use merlin::Transcript;
-    use rand::thread_rng;
-    use rug::rand::RandState;
-    use rug::Integer;
-    use std::cell::RefCell;
 
+
+fn prove(value: &Integer) {
     const LARGE_PRIMES: [u64; 4] = [
         553_525_575_239_331_913,
         12_702_637_924_034_044_211,
         378_373_571_372_703_133,
         8_640_171_141_336_142_787,
     ];
+    let params = Parameters::from_security_level(128).unwrap();
+    let mut rng1 = RandState::new();
+    rng1.seed(&Integer::from(13));
+    let mut rng2 = thread_rng();
 
-    #[test]
-    fn test_e2e_prime_rsa() {
-        let params = Parameters::from_security_level(128).unwrap();
-        let mut rng1 = RandState::new();
-        rng1.seed(&Integer::from(13));
-        let mut rng2 = thread_rng();
+    let crs = crate::protocols::membership::Protocol::<
+        Rsa2048,
+        G1Projective,
+        HPProtocol<Bls12_381>,
+    >::setup(&params, &mut rng1, &mut rng2)
+    .unwrap()
+    .crs;
+    let protocol = Protocol::<Rsa2048, G1Projective, HPProtocol<Bls12_381>>::from_crs(&crs);
 
-        let crs = crate::protocols::membership::Protocol::<
-            Rsa2048,
-            G1Projective,
-            HPProtocol<Bls12_381>,
-        >::setup(&params, &mut rng1, &mut rng2)
-        .unwrap()
-        .crs;
-        let protocol = Protocol::<Rsa2048, G1Projective, HPProtocol<Bls12_381>>::from_crs(&crs);
+    let value = Integer::from(Integer::u_pow_u(
+        2,
+        (crs.parameters.hash_to_prime_bits) as u32,
+    )) - value;
+    let randomness = Integer::from(5);
+    let commitment = protocol
+        .crs
+        .crs_modeq
+        .pedersen_commitment_parameters
+        .commit(&value, &randomness)
+        .unwrap();
 
-        let value = Integer::from(Integer::u_pow_u(
-            2,
-            (crs.parameters.hash_to_prime_bits) as u32,
-        )) - &Integer::from(245);
-        let randomness = Integer::from(5);
-        let commitment = protocol
-            .crs
-            .crs_modeq
-            .pedersen_commitment_parameters
-            .commit(&value, &randomness)
-            .unwrap();
+    let accum =
+        accumulator::Accumulator::<Rsa2048, Integer, AccumulatorWithoutHashToPrime>::empty();
+    let accum = accum.add(
+        &LARGE_PRIMES
+            .iter()
+            .skip(1)
+            .map(|p| Integer::from(*p))
+            .collect::<Vec<_>>(),
+    );
 
-        let accum =
-            accumulator::Accumulator::<Rsa2048, Integer, AccumulatorWithoutHashToPrime>::empty();
-        let accum = accum.add(
-            &LARGE_PRIMES
-                .iter()
-                .skip(1)
-                .map(|p| Integer::from(*p))
-                .collect::<Vec<_>>(),
-        );
+    let accum = accum.add_with_proof(&[value.clone()]);
+    let acc = accum.0.value;
+    let w = accum.1.witness.0.value;
+    assert_eq!(Rsa2048::exp(&w, &value), acc);
 
-        let accum = accum.add_with_proof(&[value.clone()]);
-        let acc = accum.0.value;
-        let w = accum.1.witness.0.value;
-        assert_eq!(Rsa2048::exp(&w, &value), acc);
-
-        let proof_transcript = RefCell::new(Transcript::new(b"membership"));
-        let mut verifier_channel = TranscriptVerifierChannel::new(&crs, &proof_transcript);
-        let statement = Statement {
-            c_e_q: commitment,
-            c_p: acc,
-        };
-        protocol
-            .prove(
-                &mut verifier_channel,
-                &mut rng1,
-                &mut rng2,
-                &statement,
-                &Witness {
-                    e: value,
-                    r_q: randomness,
-                    w,
-                },
-            )
-            .unwrap();
-        let proof = verifier_channel.proof().unwrap();
-        let verification_transcript = RefCell::new(Transcript::new(b"membership"));
-        let mut prover_channel =
-            TranscriptProverChannel::new(&crs, &verification_transcript, &proof);
-        protocol.verify(&mut prover_channel, &statement).unwrap();
-    }
-
-    #[test]
-    fn test_e2e_prime_class_group() {
-        let params = Parameters::from_security_level(128).unwrap();
-        let mut rng1 = RandState::new();
-        rng1.seed(&Integer::from(13));
-        let mut rng2 = thread_rng();
-
-        let crs = crate::protocols::membership::Protocol::<
-            ClassGroup,
-            G1Projective,
-            HPProtocol<Bls12_381>,
-        >::setup(&params, &mut rng1, &mut rng2)
-        .unwrap()
-        .crs;
-        let protocol = Protocol::<ClassGroup, G1Projective, HPProtocol<Bls12_381>>::from_crs(&crs);
-
-        let value = Integer::from(Integer::u_pow_u(
-            2,
-            (crs.parameters.hash_to_prime_bits) as u32,
-        )) - &Integer::from(245);
-        let randomness = Integer::from(5);
-        let commitment = protocol
-            .crs
-            .crs_modeq
-            .pedersen_commitment_parameters
-            .commit(&value, &randomness)
-            .unwrap();
-
-        let accum =
-            accumulator::Accumulator::<ClassGroup, Integer, AccumulatorWithoutHashToPrime>::empty();
-        let accum = accum.add(
-            &LARGE_PRIMES
-                .iter()
-                .skip(1)
-                .map(|p| Integer::from(*p))
-                .collect::<Vec<_>>(),
-        );
-
-        let accum = accum.add_with_proof(&[value.clone()]);
-        let acc = accum.0.value;
-        let w = accum.1.witness.0.value;
-        assert_eq!(ClassGroup::exp(&w, &value), acc);
-
-        let proof_transcript = RefCell::new(Transcript::new(b"membership"));
-        let mut verifier_channel = TranscriptVerifierChannel::new(&crs, &proof_transcript);
-        let statement = Statement {
-            c_e_q: commitment,
-            c_p: acc,
-        };
-        protocol
-            .prove(
-                &mut verifier_channel,
-                &mut rng1,
-                &mut rng2,
-                &statement,
-                &Witness {
-                    e: value,
-                    r_q: randomness,
-                    w,
-                },
-            )
-            .unwrap();
-        let proof = verifier_channel.proof().unwrap();
-        let verification_transcript = RefCell::new(Transcript::new(b"membership"));
-        let mut prover_channel =
-            TranscriptProverChannel::new(&crs, &verification_transcript, &proof);
-        protocol.verify(&mut prover_channel, &statement).unwrap();
-    }
-
-    #[test]
-    fn test_e2e_hash_to_prime() {
-        struct TestHashToPrimeParameters {}
-        impl HashToPrimeHashParameters for TestHashToPrimeParameters {
-            const MESSAGE_SIZE: u16 = 254;
-        }
-
-        let params = Parameters::from_security_level(128).unwrap();
-        let mut rng1 = RandState::new();
-        rng1.seed(&Integer::from(13));
-        let mut rng2 = thread_rng();
-
-        let crs = crate::protocols::membership::Protocol::<
-            Rsa2048,
-            G1Projective,
-            HPHashProtocol<Bls12_381, TestHashToPrimeParameters>,
-        >::setup(&params, &mut rng1, &mut rng2)
-        .unwrap()
-        .crs;
-        let protocol = Protocol::<
-            Rsa2048,
-            G1Projective,
-            HPHashProtocol<Bls12_381, TestHashToPrimeParameters>,
-        >::from_crs(&crs);
-
-        let value = Integer::from(24_928_329);
-        let (hashed_value, _) = protocol.hash_to_prime(&value).unwrap();
-        let randomness = Integer::from(5);
-        let commitment = protocol
-            .crs
-            .crs_modeq
-            .pedersen_commitment_parameters
-            .commit(&hashed_value, &randomness)
-            .unwrap();
-
-        let accum =
-            accumulator::Accumulator::<Rsa2048, Integer, AccumulatorWithoutHashToPrime>::empty();
-        let accum = accum.add(
-            &LARGE_PRIMES
-                .iter()
-                .skip(1)
-                .map(|p| Integer::from(*p))
-                .collect::<Vec<_>>(),
-        );
-
-        let accum = accum.add_with_proof(&[hashed_value.clone()]);
-        let acc = accum.0.value;
-        let w = accum.1.witness.0.value;
-        assert_eq!(Rsa2048::exp(&w, &hashed_value), acc);
-
-        let proof_transcript = RefCell::new(Transcript::new(b"membership"));
-        let mut verifier_channel = TranscriptVerifierChannel::new(&crs, &proof_transcript);
-        let statement = Statement {
-            c_e_q: commitment,
-            c_p: acc,
-        };
-        protocol
-            .prove(
-                &mut verifier_channel,
-                &mut rng1,
-                &mut rng2,
-                &statement,
-                &Witness {
-                    e: value,
-                    r_q: randomness,
-                    w,
-                },
-            )
-            .unwrap();
-        let proof = verifier_channel.proof().unwrap();
-        let verification_transcript = RefCell::new(Transcript::new(b"membership"));
-        let mut prover_channel =
-            TranscriptProverChannel::new(&crs, &verification_transcript, &proof);
-        protocol.verify(&mut prover_channel, &statement).unwrap();
-    }
-}
-
-#[cfg(all(test, feature = "dalek"))]
-mod test {
-    use super::{Protocol, Statement, Witness};
-    use crate::{
-        commitments::Commitment,
-        parameters::Parameters,
-        protocols::{
-            hash_to_prime::bp::Protocol as HPProtocol,
-            membership::transcript::{TranscriptProverChannel, TranscriptVerifierChannel},
-        },
+    let proof_transcript = RefCell::new(Transcript::new(b"membership"));
+    let mut verifier_channel = TranscriptVerifierChannel::new(&crs, &proof_transcript);
+    let statement = Statement {
+        c_e_q: commitment,
+        c_p: acc,
     };
-    use accumulator::group::Rsa2048;
-    use accumulator::{group::Group, AccumulatorWithoutHashToPrime};
-    use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar};
-    use merlin::Transcript;
-    use rand::thread_rng;
-    use rug::rand::RandState;
-    use rug::Integer;
-    use std::cell::RefCell;
-
-    const LARGE_PRIMES: [u64; 4] = [
-        553_525_575_239_331_913,
-        12_702_637_924_034_044_211,
-        378_373_571_372_703_133,
-        8_640_171_141_336_142_787,
-    ];
-
-    #[test]
-    fn test_e2e_prime() {
-        let params = Parameters::from_curve::<Scalar>().unwrap().0;
-        let mut rng1 = RandState::new();
-        rng1.seed(&Integer::from(13));
-        let mut rng2 = thread_rng();
-
-        let mut crs =
-            crate::protocols::membership::Protocol::<Rsa2048, RistrettoPoint, HPProtocol>::setup(
-                &params, &mut rng1, &mut rng2,
-            )
-            .unwrap()
-            .crs;
-        let protocol = Protocol::<Rsa2048, RistrettoPoint, HPProtocol>::from_crs(&crs);
-
-        let value = Integer::from(Integer::u_pow_u(
-            2,
-            (crs.parameters.hash_to_prime_bits) as u32,
-        )) - &Integer::from(129);
-        let randomness = Integer::from(5);
-        let commitment = protocol
-            .crs
-            .crs_modeq
-            .pedersen_commitment_parameters
-            .commit(&value, &randomness)
-            .unwrap();
-
-        let accum =
-            accumulator::Accumulator::<Rsa2048, Integer, AccumulatorWithoutHashToPrime>::empty();
-        let accum = accum.add(
-            &LARGE_PRIMES
-                .iter()
-                .skip(1)
-                .map(|p| Integer::from(*p))
-                .collect::<Vec<_>>(),
-        );
-
-        let accum = accum.add_with_proof(&[value.clone()]);
-        let acc = accum.0.value;
-        let w = accum.1.witness.0.value;
-        assert_eq!(Rsa2048::exp(&w, &value), acc);
-
-        let proof_transcript = RefCell::new(Transcript::new(b"membership"));
-        crs.crs_hash_to_prime.hash_to_prime_parameters.transcript = Some(proof_transcript.clone());
-        let mut verifier_channel = TranscriptVerifierChannel::new(&crs, &proof_transcript);
-        let statement = Statement {
-            c_e_q: commitment,
-            c_p: acc,
-        };
-        protocol
-            .prove(
-                &mut verifier_channel,
-                &mut rng1,
-                &mut rng2,
-                &statement,
-                &Witness {
-                    e: value,
-                    r_q: randomness,
-                    w,
-                },
-            )
-            .unwrap();
-        let proof = verifier_channel.proof().unwrap();
-        let verification_transcript = RefCell::new(Transcript::new(b"membership"));
-        crs.crs_hash_to_prime.hash_to_prime_parameters.transcript =
-            Some(verification_transcript.clone());
-        let mut prover_channel =
-            TranscriptProverChannel::new(&crs, &verification_transcript, &proof);
-        protocol.verify(&mut prover_channel, &statement).unwrap();
-    }
+    protocol
+        .prove(
+            &mut verifier_channel,
+            &mut rng1,
+            &mut rng2,
+            &statement,
+            &Witness {
+                e: value,
+                r_q: randomness,
+                w,
+            },
+        )
+        .unwrap();
+    let proof = verifier_channel.proof().unwrap();
+    let verification_transcript = RefCell::new(Transcript::new(b"membership"));
+    let mut prover_channel =
+        TranscriptProverChannel::new(&crs, &verification_transcript, &proof);
+    protocol.verify(&mut prover_channel, &statement).unwrap();
 }
+
+
